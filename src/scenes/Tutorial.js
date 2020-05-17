2d7baf9abdc304542e04f8d579dc6262b9ff2f03
Noel Calinawan <ncalinaw@ucsc.edu>
2020-05-17 02:14:18 -0700

    Added readable sign and mole dig placeholder

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -1,6 +1,34 @@
 class Tutorial extends Phaser.Scene{
     constructor(){
         super("tutorialScene");
+
+        
+        // dialog constants
+        this.DBOX_X = 1250;			    // dialog box x-position
+        this.DBOX_Y = 30;			    // dialog box y-position
+        this.DBOX_FONT = 'gem';	// dialog box font key
+
+        this.TEXT_X = 1300;			// text w/in dialog box x-position
+        this.TEXT_Y = 75;			// text w/in dialog box y-position
+        this.TEXT_SIZE = 24;		// text font size (in pixels)
+        this.TEXT_MAX_WIDTH = 715;	// max width of text within box
+
+        this.NEXT_TEXT = '[SPACE]';	// text to display for next prompt
+        this.NEXT_X = 775;			// next text prompt x-position
+        this.NEXT_Y = 574;			// next text prompt y-position
+
+        this.LETTER_TIMER = 10;		// # ms each letter takes to "type" onscreen
+
+        // dialog variables
+        this.dialogConvo = 0;			// current "conversation"
+        this.dialogLine = 0;			// current line of conversation
+        this.dialogSpeaker = null;		// current speaker
+        this.dialogLastSpeaker = null;	// last speaker
+        this.dialogTyping = false;		// flag to lock player input while text is "typing"
+        this.dialogText = null;			// the actual dialog text
+        this.nextText = null;			// player prompt text to continue typing
+
+     
     }
 
     preload(){
@@ -17,6 +45,10 @@ class Tutorial extends Phaser.Scene{
         this.load.image('land','./assets/bgs/land.png');
         this.load.image('sky','./assets/bgs/sky.png');
         //this.load.image('distortion','./assets/distortion.png');
+
+        this.load.json('dialogue', './assets/other/dialogue.json');
+        this.load.image('dialoguebox', './assets/other/textbox.png');
+        this.load.bitmapFont('gem', './assets/font/gem.png', './assets/font/gem.xml');
     }
 
     create(){
@@ -62,6 +94,12 @@ class Tutorial extends Phaser.Scene{
         this.sign.setCollideWorldBounds(true);
         this.keyHint = this.add.sprite(this.sign.x + 20, 310,'stuff', 'keyboard_button').setScale(0.2).setOrigin(0,0);
         this.nearSign = false;
+
+        this.dialog = this.cache.json.get('dialogue');
+        this.dialogbox = this.add.sprite(this.DBOX_X, this.DBOX_Y, 'dialoguebox').setOrigin(0);
+        this.dialogText = this.add.bitmapText(this.TEXT_X, this.TEXT_Y, this.DBOX_FONT, '', this.TEXT_SIZE);
+        this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, '', this.TEXT_SIZE);
+        this.dialogbox.visible = false;
 
         //grass animation [ foreground ]
         this.anims.create({
@@ -121,6 +159,8 @@ class Tutorial extends Phaser.Scene{
         this.mole = this.physics.add.sprite(40, 500, 'mole', 'walk_1').setScale(0.38);
         this.mole.setCollideWorldBounds(true);
 
+        this.isDigging = false;
+
         this.cat = this.physics.add.sprite(340, 500, 'cat', 'walk_1').setScale(0.33);
         this.cat.setCollideWorldBounds(true);
 
@@ -129,10 +169,13 @@ class Tutorial extends Phaser.Scene{
         this.cameras.main.setZoom(1);
         this.cameras.main.startFollow(this.frog);
         
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     update(){
-        if(cursors.left.isDown){
+
+        if(cursors.left.isDown && this.isDigging == false){
             this.frog.setVelocityX(-this.velocity)
             this.frog.setFlip(true, false);
 
@@ -145,7 +188,7 @@ class Tutorial extends Phaser.Scene{
             this.frog.anims.play('frogwalk', true);
             this.mole.anims.play('molewalk', true);
             this.cat.anims.play('catwalk', true);
-        }else if(cursors.right.isDown){
+        }else if(cursors.right.isDown && this.isDigging == false){
             this.frog.setVelocityX(this.velocity)
             this.frog.resetFlip();
 
@@ -167,7 +210,7 @@ class Tutorial extends Phaser.Scene{
             this.mole.anims.play('molewalk', false);
             this.cat.anims.play('catwalk', false);
         }
-        console.log(this.nearSign);
+   
         if(this.frog.x >= this.sign.x && this.frog.x <= 1850){
            this.keyHint.alpha = 1;
            this.nearSign = true;
@@ -175,7 +218,98 @@ class Tutorial extends Phaser.Scene{
             this.keyHint.alpha = 0;
             this.nearSign = false;
         }
+    
+        if(Phaser.Input.Keyboard.JustDown(keyA) && !this.dialogTyping && this.nearSign == true) {
+            // trigger dialog
+            this.dialogbox.visible = true;
+            this.typeText();
+            this.learnDig = true;
+        }
 
+        if(Phaser.Input.Keyboard.JustDown(keyD) && this.learnDig == true){
+            this.moleDive();
+        }
+
+    }
+
+    moleDive(){
+        this.mole.y -=50
+        this.isDigging = true;
+        this.valueChange = this.time.delayedCall(1000, () => { //change value to however long dig animation is
+            this.moleDig();
+        }, null, this);
+    
+    }
+
+    moleDig(){
+        if(this.isDigging == true){
+            this.isDigging = false;
+        }
+    }
+    typeText() {
+        // lock input while typing
+        this.dialogTyping = true;
+
+        // clear text
+        this.dialogText.text = '';
+        this.nextText.text = '';
+
+        // make sure there are lines left to read in this convo, otherwise jump to next convo
+        if(this.dialogLine > this.dialog[this.dialogConvo].length - 1) {
+            this.dialogLine = 0;
+            // I increment conversations here, but you could create logic to exit the dialog here
+            this.dialogConvo++;
+        }
+        
+        // make sure we haven't run out of conversations...
+        if(this.dialogConvo >= this.dialog.length) {
+            // here I'm simply "exiting" the last speaker and removing the dialog box,
+            // but you could build other logic to change game states here
+            console.log('End of Conversations');
+
+            // make text box invisible
+            this.dialogbox.visible = false;
+
+        } else {
+            // if not, set current speaker
+            this.dialogSpeaker = this.dialog[this.dialogConvo][this.dialogLine]['speaker'];
+
+            // build dialog (concatenate speaker + line of text)
+            this.dialogLines = this.dialog[this.dialogConvo][this.dialogLine]['speaker'].toUpperCase() + ': ' + this.dialog[this.dialogConvo][this.dialogLine]['dialog'];
+
+            // create a timer to iterate through each letter in the dialog text
+            let currentChar = 0; 
+            this.textTimer = this.time.addEvent({
+                delay: this.LETTER_TIMER,
+                repeat: this.dialogLines.length - 1,
+                callback: () => { 
+                    // concatenate next letter from dialogLines
+                    this.dialogText.text += this.dialogLines[currentChar];
+                    // advance character position
+                    currentChar++;
+                    // check if timer has exhausted its repeats 
+                    // (necessary since Phaser 3 no longer seems to have an onComplete event)
+                    if(this.textTimer.getRepeatCount() == 0) {
+                        // show prompt for more text
+                        this.nextText = this.add.bitmapText(this.NEXT_X, this.NEXT_Y, this.DBOX_FONT, this.NEXT_TEXT, this.TEXT_SIZE).setOrigin(1);
+                        // un-lock input
+                        this.dialogTyping = false;
+                        // destroy timer
+                        this.textTimer.destroy();
+                    }
+                },
+                callbackScope: this // keep Scene context
+            });
+            
+            // set bounds on dialog
+            this.dialogText.maxWidth = this.TEXT_MAX_WIDTH;
+
+            // increment dialog line
+            this.dialogLine++;
+
+            // set past speaker
+            this.dialogLastSpeaker = this.dialogSpeaker;
+        }
     }
     
 }
