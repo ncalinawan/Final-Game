cb86d8f419fcfadedd2d771337e3293f2f9276c8
mnguy161 <63267635+mnguy161@users.noreply.github.com>
2020-05-17 22:09:11 -0700

    bonus clowning: 1st prototype campfire

--- src/scenes/LevelOne.js
+++ src/scenes/LevelOne.js
@@ -2,9 +2,15 @@ class LevelOne extends Phaser.Scene{
     constructor(){
         super("First");
     }
+    
+        preload() {
+        //mai's bonus, temporary clowning
+        this.load.image('clowning', './assets/other/clowning.png');
+    }
 
     create(){
         this.add.image(0,0, 'land').setOrigin(0,0);
+        this.add.image(0,0,'clowning').setOrigin(0,0);
     }
     
 }
