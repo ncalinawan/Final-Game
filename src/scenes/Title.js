fda8e976c13a685393abdd544a5c3930a34db257
kiara_yupangco <60279368+notorious-bee@users.noreply.github.com>
2020-05-22 23:31:05 -0700

    Combine everything to make title neater

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -4,71 +4,18 @@ class Title extends Phaser.Scene{
     }
 
     preload() {
-        this.load.atlas('title', './assets/title/title_and_names.png', './assets/title/title.json');
-        this.load.atlas('press_key', './assets/title/key_press.png', './assets/title/key_to_start.json');
-        
-        this.load.atlas('frog_title', './assets/title/frog_title_walk.png', './assets/title/frog_title.json');
-        this.load.atlas('cat_title', './assets/title/cat_title_walk.png', './assets/title/cat_title.json');
-        this.load.atlas('mole_title', './assets/title/mole_title_walk.png', './assets/title/mole_title.json');
+        this.load.atlas('title', './assets/title/title_frames.png', './assets/title/title_all.json');
     }
 
     create(){
     //temporary background (to see the animations)
     this.add.rectangle(0, 0, 3600, 600, 0xFFFFFF).setOrigin(0, 0);
 
-    //title & names
-        this.anims.create({
-            key: 'title&names',
-            frames: this.anims.generateFrameNames('title', {
-                prefix: 'title_',
-                start: 1,
-                end: 2
-            }),
-            frameRate: 5,
-            repeat: -1
-        });
-        
-    //press that G key
-        this.anims.create({
-            key: 'pressG',
-            frames: this.anims.generateFrameNames('press_key', {
-                prefix: 'key_press',
-                start: 1,
-                end: 2
-            }),
-            frameRate: 5,
-            repeat: -1
-        });
-
-    //frog walk
-        this.anims.create({
-            key: 'frog',
-            frames: this.anims.generateFrameNames('frog_title', {
-                prefix: 'frog_title',
-                start: 1,
-                end: 4
-            }),
-            frameRate: 5,
-            repeat: -1
-        });
-
-    //cat walk
-        this.anims.create({
-            key: 'cat',
-            frames: this.anims.generateFrameNames('cat_title', {
-                prefix: 'title_cat',
-                start: 1,
-                end: 4
-            }),
-            frameRate: 5,
-            repeat: -1
-        });
-
-    //mole walk
+    //title
     this.anims.create({
-        key: 'mole',
-        frames: this.anims.generateFrameNames('mole_title', {
-            prefix: 'title_mole',
+        key: 'titleAll',
+        frames: this.anims.generateFrameNames('title', {
+            prefix: 'full_title',
             start: 1,
             end: 4
         }),
@@ -76,12 +23,7 @@ class Title extends Phaser.Scene{
         repeat: -1
     });
 
-    this.add.sprite(560, 290, 'title', 'title_1').play('title&names');
-    this.add.sprite(925, 125, 'press_key', 'key_press1').play('pressG');
-        
-    this.add.sprite(755, 362, 'frog', 'frog_title1').play('frog').setScale(0.75);
-    this.add.sprite(570, 370, 'cat', 'title_cat1').play('cat').setScale(0.75);
-    this.add.sprite(390, 385, 'mole', 'title_mole1').play('mole').setScale(0.75);
+    this.add.sprite(560, 290, 'title', 'full_title1').play('titleAll');
 
     keyG = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.G);
     }
