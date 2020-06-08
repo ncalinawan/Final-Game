518d734c2bf708712b1617a6db009da2a36ced33
kiara_yupangco <60279368+notorious-bee@users.noreply.github.com>
2020-06-08 02:00:15 -0700

    Additional notes to add

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 /*
 Created by Turnip Stonks: Mai Ngyuen, Kiara Yupangco, and Noel Calinawan
 Game Title: The MS Paint Adventures of Frog (and friends)
+
++additional notes:+
+- after getting the party hats, make sure to move a little closer and press A to activate rave
+- once the scrolling screen is done in the ending scene, you can now press A to get back to the title screen
 */
 let config = {
     type: Phaser.AUTO,
